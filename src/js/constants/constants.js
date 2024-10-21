e3920f4436c141a8f6eb9268865101f88b725bd5
ChayRR12 <150862145+ChayRR12@users.noreply.github.com>
2024-10-21 14:55:50 -0300

    Criando nova constante para limite de pokemons

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,3 +1,4 @@
 export const POKE_API = "https://pokeapi.co/api/v2/pokemon";
 export const POKEMON_LIST = document.getElementById("pokemon-list");
 export const REGEX_EXTRACT_POKEMON_NUMBER = /n\/(\d+)\//;
+export const POKEMON_CARDS_LIMIT = 30;
