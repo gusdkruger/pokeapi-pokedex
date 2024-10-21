2564d8e4149b7ca85e71d045a84cdecad9dc7a9e
ChayRR12 <150862145+ChayRR12@users.noreply.github.com>
2024-10-21 14:54:37 -0300

    Aplicando função Capitalizar e Numero de pokemon

--- src/js/card/card.js
+++ src/js/card/card.js
@@ -1,17 +1,20 @@
 import { extractPokemonNumber } from "../util/util.js";
-import { POKEMON_LIST } from "../constants/constants.js"
+import { capitalizeFirstLetter } from "../util/util.js";
+import { POKEMON_LIST } from "../constants/constants.js";
 
 export function createCard(pokemon) {
-
     const index = extractPokemonNumber(pokemon.url);
+    
+    const capitalizedPokemonName = capitalizeFirstLetter(pokemon.name);
+
 
     const card = `<div class="card" style="width: 18rem;">
-                    <img loading="lazy" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png" class="card-img-top" alt="...">
+                    <img loading="lazy" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png" class="card-img-top" alt="${capitalizedPokemonName}">
                     <div class="card-body">
-                        <h5 class="card-title">${pokemon.name}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                            card's content.</p>
+                        <h5 class="card-title">${capitalizedPokemonName}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="${pokemon.url}" class="btn btn-primary">Ver mais</a>
+                        <span class="pokemon-number d-none">${index}</span>
                     </div>
                 </div>`;
 
