e1254fddf1916edddeb9396f9b88662de91de950
ChayRR12 <150862145+ChayRR12@users.noreply.github.com>
2024-10-21 14:52:01 -0300

    Carregando 30 pokemons por scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import { fetchPokemonBetween } from "./api/fetchPokeApi.js";
 import { createCard } from "./card/card.js";
+import { POKEMON_CARDS_LIMIT } from "./constants/constants.js";
 
-const {count, results, next, previous} = await fetchPokemonBetween(1, 30);
+let offset = 1;
+const { count, results } = await fetchPokemonBetween(offset, POKEMON_CARDS_LIMIT);
 
 results.forEach((pokemon) => {
     createCard(pokemon);
@@ -13,11 +15,12 @@ window.addEventListener("scroll", async () => {
         const pixelsFromEnd = 400;
         const scrollPosition = window.innerHeight + window.scrollY;
         const documentHeight = document.body.offsetHeight;
-    
+
         if (documentHeight - scrollPosition <= pixelsFromEnd) {
             canLoadMore = false;
-
-            const {count, results, next, previous} = await fetchPokemonBetween(31, 60);
+            offset += POKEMON_CARDS_LIMIT;
+            
+            const { results } = await fetchPokemonBetween(offset, POKEMON_CARDS_LIMIT);
 
             results.forEach((pokemon) => {
                 createCard(pokemon);
